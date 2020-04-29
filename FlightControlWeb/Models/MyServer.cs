f8eaeb4cb761d7f8f43689425ee338730dc7babf
Yaron Sofer <58817074+yaronso@users.noreply.github.com>
2020-04-29 22:32:27 +0300

    Add files via upload

---
+++ FlightControlWeb/Models/MyServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class MyServer
+    {
+        public string ServerId { get; set; }
+        public string ServerURL { get; set; }
+    }
+}
