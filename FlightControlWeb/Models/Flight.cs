61928d56acd6694f670c1c398ef4fb2026f5eff9
Yaron Sofer <58817074+yaronso@users.noreply.github.com>
2020-04-29 20:34:05 +0300

    Add files via upload

---
+++ FlightControlWeb/Models/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class Flight
+    {
+        public string flight_id { get; set; }
+        public double longtitude { get; set; }
+        public double latitude { get; set; }
+        public int passengers { get; set; }
+        public string company_name { get; set; }
+        public DateTime date_time { get; set; }
+        public bool is_external { get; set; }
+    }
+}
---
+++ FlightControlWeb/Models/FlightPlan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+        public class InitialLocation
+        {
+            public double longitude { get; set; }
+            public double latitude { get; set; }
+            public DateTime date_time { get; set; }
+        }
+
+        // https://www.jsonutils.com/
+        public class FlightPlan
+        {
+            public int passengers { get; set; }
+            public string company_name { get; set; }
+            public InitialLocation initial_location { get; set; }
+            public IList<object> segments { get; set; }   // segments: lon, lat, time per sec, etc.
+        }
+
+    
+}
