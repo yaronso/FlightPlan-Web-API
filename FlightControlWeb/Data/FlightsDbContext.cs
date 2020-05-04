b25322b7bd807724d4086291cf8515480988d09e
liad2508 <58739913+liad2508@users.noreply.github.com>
2020-05-04 16:11:34 +0300

    Add files via upload

---
+++ FlightControlWeb/Data/FlightsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FlightControlWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Data
+{
+    public class FlightsDbContext : DbContext
+    {
+       public FlightsDbContext(DbContextOptions<FlightsDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+
+    }
+}
