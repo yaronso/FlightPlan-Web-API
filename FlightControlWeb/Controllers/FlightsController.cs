dcde0e46115b1dd9b967932cc134071f3027c878
liad2508 <58739913+liad2508@users.noreply.github.com>
2020-05-04 16:14:53 +0300

    Add files via upload

---
+++ FlightControlWeb/Controllers/FlightsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightControlWeb.Data;
+using FlightControlWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightControlWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        FlightsDbContext flightDbContext;
+        string format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+        CultureInfo provider = CultureInfo.InvariantCulture;
+
+
+        public FlightsController(FlightsDbContext _FlightsDbContext)
+        {
+            this.flightDbContext = _FlightsDbContext;
+            
+        }
+
+
+        // GET: api/Flights
+        [HttpGet]        
+        public  IEnumerable<Flight> Get()
+        {
+            //return flightDbContext.Flights.Where(m=>m.ParseDateTime(m.flight_id) > ParseDateTime(date));
+            return flightDbContext.Flights;
+        }
+
+        [HttpGet("{date}", Name = "Get")]
+        public IEnumerable<Flight> Get(string date)
+        {
+            return flightDbContext.Flights.Where
+                (m => m.date_time < ParseDateTime(date));
+        }
+
+        /*
+        // GET: api/Flights/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(string id)
+        {
+            var flight = flightDbContext.Flights.SingleOrDefault(m=>m.flight_id.Equals(id));
+            if (flight == null)
+            {
+                return NotFound("No Record Found");
+            }
+            //ParseDateTime("2020-12-26T23:56:21Z");
+            return Ok(flight);
+        }*/
+
+        // POST: api/Flights
+        [HttpPost]
+        public IActionResult Post([FromBody] Flight f)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            flightDbContext.Flights.Add(f);
+            //flightDbContext.SaveChangesAsync
+            flightDbContext.SaveChanges(true);
+            return StatusCode(StatusCodes.Status201Created);
+        } 
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var flight = flightDbContext.Flights.Find(id);
+            if (flight == null)
+            {
+                return NotFound("No Record Found");
+            }
+            flightDbContext.Flights.Remove(flight);
+            flightDbContext.SaveChanges(true);
+            return Ok("Flight Deleted"); 
+        }
+
+        private DateTime ParseDateTime(string d)
+        {
+            DateTime res;
+            res = DateTime.ParseExact(d, this.format, this.provider);
+            Debug.WriteLine(res.ToString());
+            return res;
+        }
+
+     
+    }
+}
