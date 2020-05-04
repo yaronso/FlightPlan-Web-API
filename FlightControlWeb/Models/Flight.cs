4b42bf369a26feb99b4307b717121fd255dba5a7
liad2508 <58739913+liad2508@users.noreply.github.com>
2020-05-04 16:12:18 +0300

    Add files via upload

--- FlightControlWeb/Models/Flight.cs
+++ FlightControlWeb/Models/Flight.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FlightControlWeb.Models
 {
     public class Flight
-    {
+    {   
+
+        [Key]
         public string flight_id { get; set; }
         public double longtitude { get; set; }
         public double latitude { get; set; }
         public int passengers { get; set; }
         public string company_name { get; set; }
         public DateTime date_time { get; set; }
-        public bool is_external { get; set; }
+        public bool is_external { get; set; }    
     }
 }
